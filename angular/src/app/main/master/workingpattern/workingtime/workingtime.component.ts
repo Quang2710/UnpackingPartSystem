d3f0b798d5e38f876445b8c6d45af2063de0563e
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-05-04 17:46:32 +0700

    add ag-grid

--- angular/src/app/main/master/workingpattern/workingtime/workingtime.component.ts
+++ angular/src/app/main/master/workingpattern/workingtime/workingtime.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { PaginationParamsModel } from '@app/shared/common/models/base.model';
+import { MstWptWorkingTimeServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
-  selector: 'app-workingtime',
-  templateUrl: './workingtime.component.html',
-  styleUrls: ['./workingtime.component.less']
+    selector: 'app-workingtime',
+    templateUrl: './workingtime.component.html',
+    styleUrls: ['./workingtime.component.less']
 })
 export class WorkingtimeComponent implements OnInit {
 
-  constructor() { }
+    paginationParams: PaginationParamsModel = {
+        pageNum: 1,
+        pageSize: 20,
+        totalCount: 0,
+        skipCount: 0,
+        sorting: '',
+        totalPage: 1,
+    };
 
-  ngOnInit() {
-  }
+    shiftNo: number = 0;
+    shopId: number = 0;
+    shopName: string = '';
+    workingType: number = 0;
+    startTime: any;
+    endTime: any;
+    description: string = '';
+    patternHId: number = 0;
+    seasonType: string = '';
+    dayOfWeek: string = '';
+    weekWorkingDays: number = 0;
+    isActive: string = '';
+
+    constructor(
+        injector: Injector,
+        private _service: MstWptWorkingTimeServiceProxy,
+
+    ) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    getDatas(paginationParams?: PaginationParamsModel) {
+        return this._service.getAll(
+            this.shiftNo,
+            this.shopId,
+            this.workingType,
+            this.description,
+            this.patternHId,
+            this.seasonType,
+            this.dayOfWeek,
+            this.weekWorkingDays,
+            this.isActive,
+            '',
+            this.paginationParams.skipCount,
+            this.paginationParams.pageSize
+        );
+    }
 
 }
--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -52,6 +52,10 @@ import { ZeroTemplateHttpConfigurationService } from './zero-template-http-confi
         ApiServiceProxies.EntityDynamicParameterServiceProxy,
         ApiServiceProxies.DynamicParameterValueServiceProxy,
         ApiServiceProxies.EntityDynamicParameterValueServiceProxy,
+
+
+        ApiServiceProxies.MstWptWorkingTimeServiceProxy,
+
         { provide: RefreshTokenService, useClass: ZeroRefreshTokenService },
         { provide: AbpHttpConfigurationService, useClass: ZeroTemplateHttpConfigurationService },
         { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true }
