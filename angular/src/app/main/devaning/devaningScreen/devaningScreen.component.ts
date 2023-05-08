5fcf233d45a90bd32df7a63a6670336e72f92c14
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-05-08 16:55:26 +0700

    update Devaning

--- angular/src/app/main/devaning/devaningScreen/devaningScreen.component.ts
+++ angular/src/app/main/devaning/devaningScreen/devaningScreen.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-devaningScreen',
   templateUrl: './devaningScreen.component.html',
-  styleUrls: ['./devaningScreen.component.css']
+  styleUrls: ['./devaningScreen.component.less']
 })
 export class DevaningScreenComponent implements OnInit {
+
+  process: number = 5;
 
   constructor() { }
 
   ngOnInit() {
+    this.fornumbersRange();
   }
+
+  fornumbersRange() {
+    let numRange: number[] = [];
+    for (let i = 1; i <= 5; i++) {
+      numRange.push(i);
+    }
+    return numRange;
+  }
+
 
 }
