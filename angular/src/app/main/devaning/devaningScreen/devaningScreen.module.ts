1343e258871ab026445696363aa6fd9e89d6aff7
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-05-08 22:38:11 +0700

    update devaningScreen

--- angular/src/app/main/devaning/devaningScreen/devaningScreen.module.ts
+++ angular/src/app/main/devaning/devaningScreen/devaningScreen.module.ts
@@ -1,18 +1,20 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 //import {AppSharedModule} from '@app/shared/app-shared.module';
-import {  DevaningScreenRoutingModule } from './devaningScreen-routing.module';
+import { DevaningScreenRoutingModule } from './devaningScreen-routing.module';
 import { DevaningScreenComponent } from './devaningScreen.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
         DevaningScreenComponent,
     ],
     imports: [
+        CommonModule,
         DevaningScreenRoutingModule,
         //AppSharedModule,
     ]
 })
-export class  DevaningScreenModule {}
+export class DevaningScreenModule { }
 
 
 
