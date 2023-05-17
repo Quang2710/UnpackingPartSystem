5339c958b1db1f736d8a37aa7001c796351c98da
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2023-05-18 00:33:36 +0700

    create PcHome

--- angular/src/app/main/pc/pchome/pchome.component.ts
+++ angular/src/app/main/pc/pchome/pchome.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
 
 @Component({
   selector: 'app-pchome',
   templateUrl: './pchome.component.html',
   styleUrls: ['./pchome.component.less']
 })
-export class PcHomeComponent implements OnInit {
-
-  constructor() { }
-
+export class PcHomeComponent extends AppComponentBase implements OnInit {
+  rowdata;
+  constructor(
+    injector: Injector,
+  ) {
+    super(injector)
+  }
   ngOnInit() {
   }
 
--- angular/src/app/main/pc/pchome/pchome.module.ts
+++ angular/src/app/main/pc/pchome/pchome.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PcHomeComponent } from './pchome.component';
 import { PcHomeRoutingModule } from './pchome-routing.module';
+import { AppCommonModule } from '@app/shared/common/app-common.module';
+import { TableModule } from 'primeng';
 
 @NgModule({
     declarations: [
         PcHomeComponent,
     ],
     imports: [
+        AppCommonModule,
+        TableModule,
+        CommonModule,
         CommonModule,
         PcHomeRoutingModule,
         //AppSharedModule,
