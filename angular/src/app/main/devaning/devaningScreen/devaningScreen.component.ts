3a44859eddb7b66ce72106fabb329aeeb1cae61f
Quang Vu Ngoc <93536985+Quang2710@users.noreply.github.com>
2023-12-20 10:55:59 +0700

    progress devaning

--- angular/src/app/main/devaning/devaningScreen/devaningScreen.component.ts
+++ angular/src/app/main/devaning/devaningScreen/devaningScreen.component.ts
@@ -1,4 +1,5 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, ElementRef, Injector, NgZone, OnInit, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { DevaningContModuleServiceProxy } from '@shared/service-proxies/service-proxies';
 @Component({
@@ -24,19 +25,34 @@ export class DevaningScreenComponent extends AppComponentBase implements OnInit 
     currentId;
     dateNow;
     arrayTest: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    private currentIndex = 0;
+    private interval: any;
 
     constructor(
         injector: Injector,
         private _service: DevaningContModuleServiceProxy,
+        private renderer: Renderer2,
+        private el: ElementRef,
+        private router: Router,
+        private ngZone: NgZone
     ) {
         super(injector)
     }
 
     ngOnInit() {
         this.fornumbersRange();
-        //this.loadFormData();
-        // this.getDevaningPlan();
-        // this.getDataScreen();
+        this.startColorChangeInterval();
+        // Thiết lập ngZone để k làm ảnh hưởng đến việc Devaning Container sau khi chuyển Router
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+              this.ngZone.run(() => {
+                this.resetColor();
+                this.currentIndex = 0;
+                this.startColorChangeInterval();
+              });
+            }
+          });
+
     }
     ngAfterViewInit() {
         this.getDevaningPlan();
@@ -50,10 +66,45 @@ export class DevaningScreenComponent extends AppComponentBase implements OnInit 
         // clearTimeout(this.clearTimeLoadData);
     }
 
+    startColorChangeInterval() {
+        this.interval = setInterval(() => {
+            this.changeColor();
+        }, 5000);
+    }
 
+    changeColor() {
+        const divElements = this.el.nativeElement.querySelectorAll('.plan_actual_item');
+
+        if (this.currentIndex < divElements.length) {
+            this.renderer.setStyle(
+                divElements[this.currentIndex],
+                'background-color',
+                'green'
+            );
+            this.currentIndex++;
+        } else {
+            clearInterval(this.interval);
+            this.currentIndex = 0; 
+            this.resetColor();
+            this.startColorChangeInterval();
+            // this._service.finishDvnCont(this.containerCurren.id)
+            // .subscribe(() => {
+            //     this.notify.success(this.l('FINISH Successfully '));               
+            //         this.getDevaningPlan();         
+            // });
+            console.log('done');
+            
+        }
+    }
+    resetColor() {
+        const divElements = this.el.nativeElement.querySelectorAll('.plan_actual_item');
+        divElements.forEach((element: any) => {
+          this.renderer.setStyle(element, 'background-color', 'white');
+        });
+      }
     fornumbersProcess() {
         let numRange: number[] = [];
-        for (let i = 1; i <= 40; i++) {
+        for (let i = 1; i <= 100; i++) {
             numRange.push(i);
         }
         return numRange;
@@ -99,6 +150,9 @@ export class DevaningScreenComponent extends AppComponentBase implements OnInit 
                     .subscribe(() => {
                         this.notify.success(this.l('FINISH Successfully '));
                         this.getDevaningPlan();
+                        this.currentIndex = 0;
+                        this.resetColor()
+                        this.startColorChangeInterval();
                     });
             }
         });
